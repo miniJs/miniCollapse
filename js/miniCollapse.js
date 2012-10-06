46c6c556e9fc073eca2490f339547e7a1ea69ebd
Matthieu Aussaguel <matthieu.aussaguel@gmail.com>
2012-10-06 13:40:46 +1000

    Compile CoffeeScript

---
+++ js/miniCollapse.js
@@ -0,0 +1,44 @@
+
+jQuery(function() {
+  $.miniCollapse = function(element, options) {
+    var state;
+    state = '';
+    this.settings = {};
+    this.$element = $(element);
+    this.setState = function(_state) {
+      return state = _state;
+    };
+    this.getState = function() {
+      return state;
+    };
+    this.getSetting = function(key) {
+      return this.settings[key];
+    };
+    this.callSettingFunction = function(name, args) {
+      if (args == null) {
+        args = [];
+      }
+      return this.settings[name].apply(this, args);
+    };
+    this.init = function() {
+      this.settings = $.extend({}, this.defaults, options);
+      this.callSettingFunction('callback', [this.$element, this.getSetting('message')]);
+      return this.setState('ready');
+    };
+    this.init();
+    return this;
+  };
+  $.miniCollapse.prototype.defaults = {
+    message: 'hello word',
+    callback: function() {}
+  };
+  return $.fn.miniCollapse = function(options) {
+    return this.each(function() {
+      var plugin;
+      if ($(this).data('miniCollapse') === void 0) {
+        plugin = new $.miniCollapse(this, options);
+        return $(this).data('miniCollapse', plugin);
+      }
+    });
+  };
+});
